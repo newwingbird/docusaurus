69dc7b3048d3e1d86700125b40ae64b6901a9600
newwingbird <newwingbird@icloud.com>
2021-11-10 01:49:27 +0900

    rc/pages/my-react-page.js

--- src/pages/my-react-pages.js
+++ src/pages/my-react-pages.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Layout from '@theme/Layout';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function MyReactPage() {
+  const notify = () => toast("Wow so easy!");
+
+  
   return (
     <Layout>
       <h1>My React page</h1>
       <p>This is a React page</p>
+      <div>
+      <button onClick={notify}>Notify!</button>
+      <ToastContainer />
+    </div>
     </Layout>
   );
 }
